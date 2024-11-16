7699973c9f0f46f41509a1a201df8c1ba092266a
na1ara <naiarabarceloss@gmail.com>
2024-11-16 14:42:24 -0300

    esqueleto do cliente

---
+++ client.c
@@ -0,0 +1,77 @@
+#include "common.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#define BUFSZ 1024
+
+void usage(int argc, char **argv){
+    // recebe como parâmetro o IP do servidor e o porto
+	printf("usage: %s <server IP> <server port>\n", argv[0]);
+	printf("example: %s 127.0.0.1 51511\n", argv[0]);
+	exit(EXIT_FAILURE);
+}
+
+void logexit(const char *nsg){
+    perror(nsg); // imprime qual erro foi
+    exit(EXIT_FAILURE);
+}
+
+void main(int argc, char **argv){
+    if(argc < 3){
+        usage(argc, argv);
+    }
+
+    int s;
+    s = socket(AF_INET, SOCK_STREAM, 0);
+    if(s == -1){
+        // teste de erro de conexão
+        logexit("socket");
+    }
+
+    // funcao conect
+    if(0 != connect(s, addr, sizeof(addr))){
+        logexit("connect");
+    }
+
+    char addrstr[BUFSZ];
+    addrtostr(addr, addrstr, BUFSZ);
+
+    printf("connected to %s\n");
+
+    // comunicação cliente-servidor
+    char buf[BUFSZ];
+    memset(buf, 0, BUFSZ);
+    printf("mensagem> ");
+    // dado enviado para o servidor
+    fgets(buf, BUFSZ-1, stdin);
+    int count = send(s, buf, strlen(buf)+1, 0); //n de bits transmitido na rede
+    if(count != strlen(buf)+1){
+        logexit(send);
+    }
+    
+    memset(buf, 0, BUFSZ);
+    unsigned total = 0;
+    while(1){
+        // fica recebendo dados do servidor
+        // total ordena os dados dentro do buffer no caso de receber mais de 1024 bytes
+        count = recv(s, buf + total, BUFSZ-total, 0);
+        if(count == 0){
+            // só retorna 0 do recv se a conexao tiver fechada o que é um erro
+            break;
+        }
+        total += count;
+    }
+    close(s);
+
+    printf("recebemos %u bytes\n ", total);
+    puts(buf);
+
+    exit(EXIT_SUCCESS);
+}
